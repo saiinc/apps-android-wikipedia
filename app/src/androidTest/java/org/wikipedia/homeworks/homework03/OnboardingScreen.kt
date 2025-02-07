f31d8bb386e91f25b118198854ca3f0bfad9a299
Saionji <saiinc@narod.ru>
2025-02-07 18:41:31 +0300

    second page

--- app/src/androidTest/java/org/wikipedia/homeworks/homework03/OnboardingScreen.kt
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework03/OnboardingScreen.kt
@@ -5,29 +5,18 @@ import com.google.android.material.button.MaterialButton
 import com.google.android.material.tabs.TabLayout
 import org.wikipedia.views.AppTextView
 
-val skipButton = listOf(
-    MaterialButton::class.java,
-    "fragment_onboarding_skip_button",
-    "onboarding_skip"
-)
 
-val continueButton = listOf(
-    MaterialButton::class.java,
-    "fragment_onboarding_forward_button",
-    "onboarding_continue"
-)
-
-val imageLogo = listOf(
+val page1MainImage = listOf(
     AppCompatImageView::class.java,
     "imageViewCentered"
 )
 
-val primaryText = listOf(
+val page1PrimaryText = listOf(
     AppTextView::class.java,
     "primaryTextView"
 )
 
-val secondaryText = listOf(
+val page1SecondaryText = listOf(
     AppTextView::class.java,
     "secondaryTextView"
 )
@@ -43,7 +32,34 @@ val addLanguageButton = listOf(
     "onboarding_multilingual_add_language_text"
 )
 
+val skipButton = listOf(
+    MaterialButton::class.java,
+    "fragment_onboarding_skip_button",
+    "onboarding_skip"
+)
+
+val continueButton = listOf(
+    MaterialButton::class.java,
+    "fragment_onboarding_forward_button",
+    "onboarding_continue"
+)
+
 val tabPageIndicator = listOf(
     TabLayout::class.java,
     "view_onboarding_page_indicator"
+)
+
+val page2MainImage = listOf(
+    AppCompatImageView::class.java,
+    "imageViewCentered"
+)
+
+val page2PrimaryText = listOf(
+    AppTextView::class.java,
+    "primaryTextView"
+)
+
+val page2SecondaryText = listOf(
+    AppTextView::class.java,
+    "secondaryTextView"
 )
