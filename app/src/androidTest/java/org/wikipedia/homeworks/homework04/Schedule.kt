94b21a632fe473122c30312cc659de8d15169db2
Saionji <saiinc@narod.ru>
2025-02-10 00:28:31 +0300

    lesson 4

--- app/src/androidTest/java/org/wikipedia/homeworks/homework04/Schedule.kt
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework04/Schedule.kt
@@ -28,29 +28,53 @@ class Schedule {
         function()
     }
 
-    /*
-    infix fun Char(":").by(minutes: String): LocalTime {
-        return LocalTime.of(this.toInt(), minutes.toInt())
-    }*/
 
+    private var dayOfWeek = Days.FRIDAY
 
+    fun monday(function: () -> Unit) {
+        dayOfWeek = Days.MONDAY
+        function()
+    }
+
+    fun tuesday(function: () -> Unit) {
+        dayOfWeek = Days.TUESDAY
+        function()
+    }
+
+    fun wednesday(function: () -> Unit) {
+        dayOfWeek = Days.WEDNESDAY
+        function()
+    }
+
+    fun thursday(function: () -> Unit) {
+        dayOfWeek = Days.THURSDAY
+        function()
+    }
+
+    fun friday(function: () -> Unit) {
+        dayOfWeek = Days.FRIDAY
+        function()
+    }
+
+    fun saturday(function: () -> Unit) {
+        dayOfWeek = Days.SATURDAY
+        function()
+    }
+
+    fun sunday(function: () -> Unit) {
+        dayOfWeek = Days.SUNDAY
+        function()
+    }
 
 
     operator fun String.rangeTo(endTime: String): Pair<LocalTime, LocalTime> {
-        val stringStartTime = this.split(":")
-        val intStartTime = LocalTime.of(stringStartTime[0].toInt(), stringStartTime[1].toInt())
-        val stringEndTime = endTime.split(":")
-        val intEndTime = LocalTime.of(stringEndTime[0].toInt(), stringEndTime[1].toInt())
-        return Pair<LocalTime, LocalTime>(intStartTime, intEndTime)
+        return Pair<LocalTime, LocalTime>(LocalTime.parse(this), LocalTime.parse(endTime))
     }
 
-    infix fun Pair<LocalTime, LocalTime>.schedule(lesson: String): ScheduleEntity {
-        return ScheduleEntity(lesson, this.first, this.second)
+    infix fun Pair<LocalTime, LocalTime>.schedule(lesson: String) {
+        addSchedule(dayOfWeek, ScheduleEntity(lesson, this.first, this.second))
     }
 
-    /* infix fun String.ScheduleEntity(scheduleEntity: ScheduleEntity) {
-        addSchedule(wednesday, scheduleEntity)
-    }*/
 
     override fun toString(): String {
         return scheduleOfWeek.toSortedMap()
@@ -86,60 +110,45 @@ fun main() {
         ScheduleEntity("Chemistry", LocalTime.of(11, 15), LocalTime.of(11, 55))
     )
 
-// Промежуточный вариант
-    schedule {
-        addSchedule(
-            Days.TUESDAY,
-            ScheduleEntity("History", LocalTime.of(10, 30), LocalTime.of(11, 10))
-        )
-        addSchedule(
-            Days.TUESDAY,
-            ScheduleEntity("Literature", LocalTime.of(11, 15), LocalTime.of(11, 55))
-        )
-        addSchedule(
-            Days.TUESDAY,
-            "10:30".."11:10" schedule "Biology"
-        )
-    }
 
 // Так добавляется расписание с использованием DSL
-//    schedule {
-//
-//        monday {
-//            "10:30".."11:10" schedule "Biology"
-//            "11:15".."11:55" schedule "Chemistry"
-//            "09:00".."09:40" schedule "Mathematics"
-//            "09:45".."10:25" schedule "History"
-//        }
-//
-//        tuesday {
-//            "09:00".."09:40" schedule "English"
-//            "09:45".."10:25" schedule "Geography"
-//            "11:15".."11:55" schedule "Art"
-//            "10:30".."11:10" schedule "Physics"
-//        }
-//
-//        wednesday {
-//            "11:15".."11:55" schedule "Biology"
-//            "09:00".."09:40" schedule "Literature"
-//            "10:30".."11:10" schedule "History"
-//            "09:45".."10:25" schedule "Mathematics"
-//        }
-//
-//        thursday {
-//            "11:15".."11:55" schedule "Physics"
-//            "10:30".."11:10" schedule "Geography"
-//            "09:00".."09:40" schedule "Chemistry"
-//            "09:45".."10:25" schedule "English"
-//        }
-//
-//        friday {
-//            "09:45".."10:25" schedule "Literature"
-//            "11:15".."11:55" schedule "History"
-//            "09:00".."09:40" schedule "Art"
-//            "10:30".."11:10" schedule "Mathematics"
-//        }
-//    }
+    schedule {
+
+        monday {
+            "10:30".."11:10" schedule "Biology"
+            "11:15".."11:55" schedule "Chemistry"
+            "09:00".."09:40" schedule "Mathematics"
+            "09:45".."10:25" schedule "History"
+        }
+
+        tuesday {
+            "09:00".."09:40" schedule "English"
+            "09:45".."10:25" schedule "Geography"
+            "11:15".."11:55" schedule "Art"
+            "10:30".."11:10" schedule "Physics"
+        }
+
+        wednesday {
+            "11:15".."11:55" schedule "Biology"
+            "09:00".."09:40" schedule "Literature"
+            "10:30".."11:10" schedule "History"
+            "09:45".."10:25" schedule "Mathematics"
+        }
+
+        thursday {
+            "11:15".."11:55" schedule "Physics"
+            "10:30".."11:10" schedule "Geography"
+            "09:00".."09:40" schedule "Chemistry"
+            "09:45".."10:25" schedule "English"
+        }
+
+        friday {
+            "09:45".."10:25" schedule "Literature"
+            "11:15".."11:55" schedule "History"
+            "09:00".."09:40" schedule "Art"
+            "10:30".."11:10" schedule "Mathematics"
+        }
+    }
 
     println(schedule.toString())
 }
